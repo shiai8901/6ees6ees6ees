ee9e98b6e96c0b582e2a71a94dc4e77f22762884
Ai Shi <shiai8901@gmail.com>
2016-12-20 16:00:06 -0800

    complete

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,14 @@
-class ForagerBee {
+class ForagerBee extends Bees {
   // TODO..
+  constructor() {
+    super();
+    this.age = 10
+    this.job = 'find pollen'
+    this.canFly = true
+    this.treasureChest = []
+  }
+
+  forage(treasure) {
+    this.treasureChest.push(treasure);
+  }
 };
--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,17 @@
-class HoneyMakerBee {
+class HoneyMakerBee extends Bees{
   // TODO..
+  constructor() {
+    super();
+    this.eage = 10
+    this.job = 'make honey'
+    this.honeyPot = 0
+  }
+
+  makeHoney() {
+    this.honeyPot += 1
+  }
+
+  giveHoney() {
+    this.honeyPot -= 1
+  }
 };
